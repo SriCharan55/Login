20a28fb26023a7a207a1d9114d5de7c9a8d60405
xyz <xyz.@example.com>
2026-03-05 11:58:24 +0530

    chnged styles

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://login-snyp.onrender.com"
 });
 
 export const loginUser = (data) => {
